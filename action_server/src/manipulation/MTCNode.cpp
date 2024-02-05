76d39ed19debfe653e164dcd8433526aec90ab52
rperez <rodrigo.perez@urjc.es>
2024-02-05 12:42:05 +0100

    first commit

---
+++ action_server/src/manipulation/MTCNode.cpp
@@ -0,0 +1,77 @@
+#include "manipulation/MTCNode.hpp"
+
+
+namespace manipulation
+{
+
+MTCNode::MTCNode(const rclcpp::NodeOptions& options, std::string group, std::string goal)
+  : group_(group),
+    goal_(goal) 
+{
+  node_ = std::make_shared<rclcpp::Node>("moveit::task_constructor_node", options);
+}
+
+rclcpp::node_interfaces::NodeBaseInterface::SharedPtr
+MTCNode::get_node_base_interface()
+{
+  return node_->get_node_base_interface();
+}
+
+void
+MTCNode::do_task()
+{
+  task_ = create_task();
+
+  try
+  {
+    task_.init();
+  }
+  catch (moveit::task_constructor::InitStageException& e)
+  {
+    RCLCPP_ERROR(node_->get_logger(), "Initialization failed: %s", e.what());
+    return;
+  }
+
+  if (!task_.plan(5)) // creates 5 solutions
+  {
+    RCLCPP_ERROR(node_->get_logger(), "Task planning failed");
+    return;
+  }
+  // this for visualization in RViz
+  task_.introspection().publishSolution(*task_.solutions().front());
+
+  auto result = task_.execute(*task_.solutions().front());
+  if (result.val != moveit_msgs::msg::MoveItErrorCodes::SUCCESS)
+  {
+    RCLCPP_ERROR(node_->get_logger(), "Task execution failed");
+    return;
+  }
+
+  return;
+}
+
+moveit::task_constructor::Task
+MTCNode::create_task(){ 
+  moveit::task_constructor::Task task;
+  
+  task.stages()->setName("move to pose");
+  task.loadRobotModel(node_);
+
+
+  auto stage_state_current = std::make_unique<moveit::task_constructor::stages::CurrentState>("current");
+  task.add(std::move(stage_state_current));
+
+  auto interpolation_planner = std::make_shared<moveit::task_constructor::solvers::JointInterpolationPlanner>();
+  
+  auto stage =
+      std::make_unique<moveit::task_constructor::stages::MoveTo>("given pose", interpolation_planner);
+
+  stage->setGroup(group_);
+  stage->setGoal(goal_);
+
+  task.add(std::move(stage));
+
+  return task;
+
+}
+} // namespace manipulation
---
+++ action_server/src/manipulation/MoveToActionServer.cpp
@@ -0,0 +1,80 @@
+#include "manipulation/MoveToActionServer.hpp"
+
+
+namespace manipulation
+{
+
+MoveToActionServer::MoveToActionServer()
+: Node("manipulation_action_server")
+{
+  action_server_ = rclcpp_action::create_server<MoveTo>(
+    this,
+    "move_robot_arm",
+    std::bind(&MoveToActionServer::handle_goal, this, std::placeholders::_1, std::placeholders::_2),
+    std::bind(&MoveToActionServer::handle_cancel, this, std::placeholders::_1),
+    std::bind(&MoveToActionServer::handle_accepted, this, std::placeholders::_1)
+  );
+
+}
+
+rclcpp_action::GoalResponse
+MoveToActionServer::handle_goal(
+    const rclcpp_action::GoalUUID & uuid,
+    std::shared_ptr<const manipulation_action::action::MoveTo::Goal> goal)
+{
+  goal_ = goal->goal_pose;
+  group_ = goal->group_name;
+  
+  RCLCPP_INFO(this->get_logger(), "Received goal to move robot art to pose %s", goal_.c_str());
+  (void)uuid;
+  return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+rclcpp_action::CancelResponse
+MoveToActionServer::handle_cancel(
+    const std::shared_ptr<rclcpp_action::ServerGoalHandle<manipulation_action::action::MoveTo>> goal_handle)
+{
+  RCLCPP_INFO(this->get_logger(), "Goal cancelled");
+  (void)goal_handle;
+  return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+void
+MoveToActionServer::handle_accepted(
+    const std::shared_ptr<GoalHandleMove> goal_handle)
+{
+  RCLCPP_INFO(this->get_logger(), "Goal accepted");
+  
+  std::thread{std::bind(&MoveToActionServer::move_robot_arm, this, std::placeholders::_1),
+    goal_handle}.detach();
+
+}
+
+void
+MoveToActionServer::move_robot_arm(const std::shared_ptr<GoalHandleMove> goal_handle)
+{
+  // auto feedback = std::make_shared<MoveTo::Feedback>();
+  auto result = std::make_shared<MoveTo::Result>();
+
+  rclcpp::NodeOptions options;
+  options.automatically_declare_parameters_from_overrides(true);
+
+  auto mtc_node = std::make_shared<MTCNode>(options, group_, goal_);
+  // rclcpp::executors::MultiThreadedExecutor executor;
+  
+  // auto spin_thread = std::make_unique<std::thread>([&executor, &mtc_node]() {
+  //   executor.add_node(mtc_node->get_node_base_interface());
+  //   executor.spin();
+  //   executor.remove_node(mtc_node->get_node_base_interface());
+  // });
+
+  // mtc_node->do_task();
+
+  // spin_thread->join();
+  
+  // result->success = true;
+  // RCLCPP_INFO(this->get_logger(), "Goal succeeded");
+  goal_handle->succeed(result);
+}
+
+} // namespace manipulation
